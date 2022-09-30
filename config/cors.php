41122958f4f1033029a9db4ed33ed9a077e50203
Sushan Tachamo <sushantachamo@Sushans-MacBook-Pro.local>
2022-09-30 09:42:36 +0545

    check CROS

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
@@ -25,7 +25,7 @@ return [
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => FALSE,
 
     'max_age' => 0,
 
